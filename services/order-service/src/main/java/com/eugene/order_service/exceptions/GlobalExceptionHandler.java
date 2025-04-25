e48a4baad32ed99f9388554a05a65e5a53206949
Eugene-Kwaka <kwakaeugene@gmail.com>
2025-04-24 19:55:32 -0500

    Made some changes and confirmed that the app is running perfectly on my local machine.

--- services/order-service/src/main/java/com/eugene/order_service/exceptions/GlobalExceptionHandler.java
+++ services/order-service/src/main/java/com/eugene/order_service/exceptions/GlobalExceptionHandler.java
@@ -1,15 +1,15 @@
 package com.eugene.order_service.exceptions;
 
-import jakarta.persistence.EntityNotFoundException;
-import jakarta.validation.ConstraintViolationException;
+// import jakarta.persistence.EntityNotFoundException;
+// import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+// import org.springframework.validation.FieldError;
+// import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.util.HashMap;
-import java.util.Map;
+// import java.util.HashMap;
+// import java.util.Map;
 
 public class GlobalExceptionHandler {
 
